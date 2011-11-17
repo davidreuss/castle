9ac4f3c7832f47fcdfb810f2e3e912b97454199d
Henrik Bjørnskov <henrik@bearwoods.dk>
2011-11-17 10:23:01 +0100

    Nice output. Install.sh should run bundles.sh

--- install.sh
+++ install.sh
@@ -4,17 +4,20 @@ dotfiles=( "vim" "vimrc" "gvimrc" "inputrc" "zsh" "zshenv" "zshrc" )
 
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo "> Installing dotfiles"
+
 for dotfile in ${dotfiles[@]};do
     source="$DIR/$dotfile"
     destination="$HOME/.$dotfile"
 
     if [ ! -f $destination ] && [ ! -d $destination ]; then
-        echo "> Installing $source as $destination"
+        echo ">> Installing $source as $destination"
         ln -s ${source} ${destination}
-    else
-        echo "> $destination already exists"
     fi
 done;
 
+mkdir "$DIR/vim/bundle" &>/dev/null
 
-mkdir "$DIR/vim/bundle"
+echo ""
+
+$DIR/bundles.sh
