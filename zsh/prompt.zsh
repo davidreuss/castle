6318d2005ca27521b02d83e60f0d523a082bc22f
Henrik Bjørnskov <henrik@bjrnskov.dk>
2012-09-03 19:30:37 +0200

    Show unstaged commits

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,6 +2,6 @@ precmd() {
     vcs_info
 }
 
-PROMPT='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}%~%{$reset_color%}${vcs_info_msg_0_} '
+PROMPT='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%m:"}%{$fg_bold[blue]%}${PWD/#$HOME/~}%{$reset_color%}${vcs_info_msg_0_} '
 
 bindkey -v
--- zsh/vcs.zsh
+++ zsh/vcs.zsh
@@ -1,7 +1,8 @@
 autoload -U vcs_info
 
-zstyle ':vcs_info:*' enable git cvs
+zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' use-simple true
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' unstagedstr "+" # uncommitted changes
-zstyle ':vcs_info:*' formats " (%{$fg_bold[green]%}%b%{$reset_color%}%{$fg_bold[red]%}%u%{$reset_color%})"
+zstyle ':vcs_info:*' stagedstr "+" # staged commits
+zstyle ':vcs_info:*' formats " (%{$fg_bold[green]%}%b%{$reset_color%}%{$fg_bold[red]%}%u%{$reset_color%}%{$fg_bold[red]%}%c%{$reset_color%})"
