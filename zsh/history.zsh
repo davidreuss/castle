184b5672d7f8912940dc51cc86e88c9fbab49cea
Henrik Bjørnskov <henrik@bjrnskov.dk>
2012-09-25 11:19:11 +0200

    Persistent history

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,4 +1,5 @@
-setopt inc_append_history share_history hist_ignore_dups
+setopt append_history inc_append_history share_history hist_ignore_dups
 
+HISTSIZE=10000
 SAVEHIST=10000
 HISTFILE=$HOME/.zsh_history
